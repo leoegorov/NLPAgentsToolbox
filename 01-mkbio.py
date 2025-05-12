3cb27ea9ce7412c9c9319ff5cbd5795779472f6f
Asil thinkbook 14+ <moonbizhi@gmail.com>
2025-05-12 12:18:37 +0300

    Cache to improve performance. 01-mkbio.py:request -> requests_cache

--- 01-mkbio.py
+++ 01-mkbio.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import requests # type: ignore
+import requests_cache
+from datetime import timedelta
 import random
 import sqlite3
 import os
@@ -9,6 +10,7 @@ import argparse
 # fallback variables
 DATABASE_FILE = os.environ.get('DATABASE_FILE', 'juror.db')
 API_CENSUS  = os.environ.get('API_CENSUS', 'https://api.census.gov/data/2020/dec/pl')
+
 # API_KEY = None  # Optional: Replace with your key like 'your_api_key_here'
 
 def check_environment_variables():
@@ -16,7 +18,7 @@ def check_environment_variables():
         print(f"Warning: DATABASE_FILE location is not set. Defaulting to {os.getcwd()}/{DATABASE_FILE}", file=sys.stderr)
 
 # Get list of U.S. states and populations
-def fetch_state_populations():
+def fetch_state_populations(session= None):
     params = {
         'get': 'NAME,P1_001N',
         'for': 'state:*'
@@ -24,7 +26,7 @@ def fetch_state_populations():
     # if API_KEY:
     #     params['key'] = API_KEY
 
-    response = requests.get(API_CENSUS, params=params)
+    response = session.get(API_CENSUS, params=params)
     data = response.json()
     headers, rows = data[0], data[1:]
     return [(row[0], int(row[1]), row[2]) for row in rows]  # (state_name, population, state_code)
@@ -42,7 +44,7 @@ def generate_random_person():
     return gender
 
 # Get list of U.S. popolation vs age
-def fetch_pop_age(gender= "m"):
+def fetch_pop_age(gender= "m", session= None):
     ageDivision= [f"B01001_{i+3:03d}E" for i in range(8, 23)]
     if gender== "f":
         ageDivision= [f"B01001_{i+3:03d}E" for i in range(8, 23)]
@@ -52,9 +54,9 @@ def fetch_pop_age(gender= "m"):
         }
     tmpAPI= "https://api.census.gov/data/2023/acs/acs1"
     variableURL= tmpAPI+"/variables.json"
-    response = requests.get(variableURL)
+    response = session.get(variableURL)
     variable = response.json()
-    response = requests.get(tmpAPI, params= params)
+    response = session.get(tmpAPI, params= params)
     agePopData = response.json()
     label= [variable["variables"][i]["label"].split("!!")[-1] for i in agePopData[0][0: -1]]
     # print(label)
@@ -63,7 +65,7 @@ def fetch_pop_age(gender= "m"):
     return agePopWeight
 
 # Get list of U.S. family vs annual income
-def fetch_family_income():
+def fetch_family_income(session= None):
     division= [f"B19101A_{i+2:03d}E" for i in range(16)]
     # print(division)
     params = {
@@ -72,9 +74,9 @@ def fetch_family_income():
         }
     tmpAPI= "https://api.census.gov/data/2023/acs/acs1"
     variableURL= tmpAPI+"/variables.json"
-    response = requests.get(variableURL)
+    response = session.get(variableURL)
     variable = response.json()
-    response = requests.get(tmpAPI, params= params)
+    response = session.get(tmpAPI, params= params)
     tmpData = response.json()
     label= [variable["variables"][i]["label"].split("!!")[-1] for i in tmpData[0][0: -1]]
     pop= [int(i) for i in tmpData[1][0: -1]]
@@ -82,7 +84,7 @@ def fetch_family_income():
     return namePopWeight
 
 # Get list of U.S. population vs one race, whole age
-def fetch_pop_singleRace(state= 1):
+def fetch_pop_singleRace(state= 1, session= None):
     division= ["P11_008N", "P9_007N", "P9_009N", "P9_005N", "P9_006N"]
     # print(division)
     params = {
@@ -91,17 +93,17 @@ def fetch_pop_singleRace(state= 1):
         }
     tmpAPI= "https://api.census.gov/data/2020/dec/cd118"
     variableURL= tmpAPI+"/variables.json"
-    response = requests.get(variableURL)
+    response = session.get(variableURL)
     variable = response.json()
     label= [variable["variables"][i]["label"].split("!!")[-1] for i in division]
-    response = requests.get(tmpAPI, params= params)
+    response = session.get(tmpAPI, params= params)
     tmpData = response.json()
     pop= [int(i) for i in tmpData[1][0: -1]]
     namePopWeight= [label, pop]
     return namePopWeight
 
 # Get list of U.S. population vs one education level, 25 and over
-def fetch_pop_education(gender= "f", state= 1):
+def fetch_pop_education(gender= "f", state= 1, session= None):
     division= [f"S1501_C05_{i+7:03d}E" for i in range(7)]
     if gender== "f":
         division= [f"S1501_C03_{i+7:03d}E" for i in range(7)]
@@ -112,17 +114,17 @@ def fetch_pop_education(gender= "f", state= 1):
         }
     tmpAPI= "https://api.census.gov/data/2023/acs/acs1/subject"
     variableURL= tmpAPI+"/variables.json"
-    response = requests.get(variableURL)
+    response = session.get(variableURL)
     variable = response.json()
     label= [variable["variables"][i]["label"].split("!!")[-1] for i in division]
     # print(label)
-    response = requests.get(tmpAPI, params= params)
+    response = session.get(tmpAPI, params= params)
     tmpData = response.json()
     pop= [int(i) for i in tmpData[1][0: -1]]
     namePopWeight= [label, pop]
     return namePopWeight
 
-def fetch_pop_occupation(gender= "f"):
+def fetch_pop_occupation(gender= "f", session= None):
     division= [f"S2401_C02_{i+2:03d}E" for i in range(35)]
     if gender== "f":
         division= [f"S2401_C04_{i+2:03d}E" for i in range(35)]
@@ -132,10 +134,10 @@ def fetch_pop_occupation(gender= "f"):
         }
     tmpAPI= "https://api.census.gov/data/2023/acs/acs1/subject"
     variableURL= tmpAPI+"/variables.json"
-    response = requests.get(variableURL)
+    response = session.get(variableURL)
     variable = response.json()
     label= [variable["variables"][i]["label"].split("!!")[-1] for i in division]
-    response = requests.get(tmpAPI, params= params)
+    response = session.get(tmpAPI, params= params)
     tmpData = response.json()
     pop= [int(i) for i in tmpData[1][0: -1]]
     namePopWeight= [label, pop]
@@ -175,26 +177,29 @@ def main():
     parser.add_argument('--version', action='version', version='mkbio v0.0')
     args = parser.parse_args()
 
+    # cache visited website for better performance
+    session = requests_cache.CachedSession('request_cache', expire_after=timedelta(hours=2))
+
     check_environment_variables()
     print("Fetching U.S. population data by state...")
-    states = fetch_state_populations()
+    states = fetch_state_populations(session)
     print("Fetching U.S. population data by state...")
     state_name, _, stateID = select_state_weighted(states)
     gender = generate_random_person()
     print("Fetching U.S. age data by gender...")
-    agePopWeight= fetch_pop_age(gender)
+    agePopWeight= fetch_pop_age(gender, session)
     age= select_name_weighted(agePopWeight)
     print("Fetching U.S. income data...")
-    incomePopWeight= fetch_family_income()
+    incomePopWeight= fetch_family_income(session)
     income= select_name_weighted(incomePopWeight)
     print("Fetching U.S. race data by state...")
-    racePopWeight= fetch_pop_singleRace(int(stateID))
+    racePopWeight= fetch_pop_singleRace(int(stateID), session)
     race= select_name_weighted(racePopWeight)
     print("Fetching U.S. education data by state and gender...")
-    eduPopWeight= fetch_pop_education(gender, int(stateID))
+    eduPopWeight= fetch_pop_education(gender, int(stateID), session)
     edu= select_name_weighted(eduPopWeight)
     print("Fetching U.S. occupation data by gender...")
-    occupationPopWeight= fetch_pop_occupation(gender)
+    occupationPopWeight= fetch_pop_occupation(gender, session)
     occupation= select_name_weighted(occupationPopWeight)
     traits= [age, gender, state_name, income, race, edu, occupation]
     print(traits)
